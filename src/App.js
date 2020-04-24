0371dd66b47dfdcbcc569ab387da3ae6a47649b9
Hassan78 <hassan.ali@g.austincc.edu>
2020-04-23 22:34:06 -0500

    removed logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './screens/home/home';
 import MealContextApiProvider from './context/mealcontext';
@@ -8,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <h2>Random Meal Generator</h2>
       </header>
       <MealContextApiProvider>
